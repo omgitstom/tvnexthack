451df719f6327cb0f54ef974378157d11760156c
Matthew McClure <mmcclure@brightcove.com>
2013-04-27 14:52:12 -0700

    added answer and question

--- srv/pubsub.js
+++ srv/pubsub.js
@@ -8,10 +8,20 @@ module.exports = {
       mainRef.child(event).child('drink').set(false);
     }, length * 1000);
   },
-  sendQuestion: function(question, answers, correctIx) {
-    var trivia = new Firebase('https://drivia.firebaseio.com/trivia');
-    newQuestion = trivia.push();
-    newQuestion.set({question: question, answers: answers, correct: correctIx});
-  },
-  answerQuestion: function() {}
+  question: {
+    trivia: new Firebase('https://drivia.firebaseio.com/trivia');
+    send: function(question, answers, correctIx) {
+      var newQuestion = this.trivia.push();
+      newQuestion.set({question: question, answers: answers, correct: correctIx});
+      this.current = newQuestion;
+      return newQuestion;
+    },
+    answer: function(question, user, answer) {
+      if (question != this.current) {
+        console.log('Not the current question');
+      } else {
+        question.users.child(user.username).set(answer);
+      }
+    }
+  }
 };
