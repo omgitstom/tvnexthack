97de79fada6db52aec1c4dfe1282d3049182a765
Matthew McClure <matt@htbx.net>
2013-04-27 18:56:30 +0000

    commented out app.gets

--- srv/app.js
+++ srv/app.js
@@ -7,7 +7,6 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-<<<<<<< HEAD
   , path = require('path')
 	, Firebase = require('firebase');
 
@@ -15,22 +14,6 @@ var app = express();
 
 var mainRef = new Firebase('https://drivia.firebaseIO-demo.com/');
 
-=======
-<<<<<<< HEAD
-  , path = require('path')
-	, Firebase = require('firebase');
-
-var app = express();
-
-var mainRef = new Firebase('https://drivia.firebaseIO-demo.com/');
-
-=======
-  , path = require('path');
-
-var app = express();
-
->>>>>>> 6fa9214f472ef9bc94d7c07f10c6e4057185137f
->>>>>>> master
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -47,8 +30,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// app.get('/', routes.index);
+// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ srv/baseball.js
@@ -0,0 +1,79 @@
+module.exports = {
+  1: [
+    "ballpark",
+   	"big league",
+    "bush-league",
+    "cleanup hitter",
+		"closer",
+    "curve",
+    "first base",
+    "first inning",
+    "early innings",
+    "ground ball",
+    "home run",
+    "out of the park",
+    "late innings",
+    "leadoff hitter",
+    "left field",
+    "major league",
+    "ninth inning",
+    "off base",
+    "on deck",
+    "one base at a time",
+  ],
+  2: [
+  	"batting 1000",
+    "brand new ballgame",
+    "brush back",
+    "cover one's bases",
+    "double header",
+    "down to the last out",
+    "extra innings",
+    "grand slam",
+    "grandstanding",
+    "hardball",
+    "heavy hitter",
+    "out of the park",
+    "déjà vu"
+  ],
+  3: [
+  	"beanball",
+    "can of corn",
+    "Charley horse",
+    "ducks on a pond",
+    "foot in the bucket",
+    "It ain't over till it's over!",
+    "cover off the ball",
+    "Mendoza line",
+    "rally cap",
+    "rain-check"
+  ]
+    
+}
+
+out of left field
+pinch hit
+pitch a shutout
+play ball
+play softball
+relief pitcher
+right off the bat
+screwball
+shutout
+singles or hit singles
+small ball
+softball
+step up to the plate
+strike
+strike out swinging
+strike out looking
+swing and miss
+swing for the fences
+switch-hitter
+three strikes law
+took the collar
+touch base
+triple play
+wheelhouse
+whiff
+whole new ball game
