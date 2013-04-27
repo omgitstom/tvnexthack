22a9388138707913d367a9fd6be15aa3f360bb54
David LaPalomento <dlapalomento@gmail.com>
2013-04-27 19:18:51 -0400

    add weight and age questions

--- srv/game.js
+++ srv/game.js
@@ -23,6 +23,11 @@ module.exports = function(){
       });
     },
 
+    sendQuestion = function(question, answers, correctIx){
+      console.log(question, answers, correctIx);
+      pubsub.question.send(question, answers, correctIx);
+    },
+
     lightningRound = function(){
       http.get('http://livefeed.api.tv/hack2013/v1/getlivefeeditems/args/livefeed/1632393/starttime/live/format.json', function(response){
         var data = '';
@@ -45,33 +50,89 @@ module.exports = function(){
       });
     },
 
+    // range is inclusive
     random = function(start, end){
       return start + Math.round((end - start) * Math.random());
     },
+    randoms = function(start, end, count){
+      var result = [], next;
+      while (result.length < count) {
+        next = random(start, end);
+        if (!~result.indexOf(next)) {
+          result.push(next);
+        }
+      }
+      return result;
+    },
 
-    weightQuestion = function(left, right){
+    questions,
+
+    chooseQuestion = function(left, right){
       return function(){
         var
           team = random(0, 1) ? left : right,
-          athlete = team[random(0, team.length)],
-          correctIx = random(0, 4),
-          i = 0,
-          answers = [];
+          athlete = team[random(0, team.length - 1)];
 
-        for (; i < 4; ++i) {
-          if (i == correctIx) {
-            answers.push(athlete.weight);
-          } else if (random(0, 1)) {
-            answers.push(athlete.weight - 5 - (5 * random(0, 3)));
-          } else {
-            answers.push(athlete.weight + 5 + (5 * random(0, 3)));
-          }
-        }
-        
-        console.log("How much does " + athlete.displayName + " weigh?", answers, correctIx);
-        pubsub.question.send("How much does " + athlete.displayName + " weigh?", answers, correctIx);
+        questions[random(0, questions.length - 1)](team, athlete);
       }
+    },
+
+    weightQuestion = function(team, athlete){
+      var
+        correctIx = random(0, 3),
+        answers = randoms(0, 3, 3);
+
+      answers = answers.map(function(e){
+        if (random(0, 1)) {
+          return athlete.weight + (5 * e) + 5;
+        } else {
+          return athlete.weight - (5 * e) - 5;
+        }
+      });
+      answers.splice(correctIx, 0, athlete.weight);
+
+      sendQuestion("How much does " + athlete.displayName + " weigh?", answers, correctIx);
+    },
+    ageQuestion = function(team, athlete){
+      var
+        correctIx = random(0, 3),
+        answers = randoms(0, 5, 3);
+
+      answers = answers.map(function(e){
+        if (random(0, 1)) {
+          return athlete.age + e + 1;
+        } else {
+          return athlete.age - e - 1;
+        }
+      });
+      answers.splice(correctIx, 0, athlete.age);
+
+      sendQuestion("How old is " + athlete.displayName + "?", answers, correctIx);
+    },
+    heightQuestion = function(team, athlete){
+      var
+        correctIx = random(0, 3),
+        answers = randoms(0, 5, 3);
+
+      answers = answers.map(function(e){
+        var height;
+        if (random(0, 1)) {
+          height = athlete.height + e + 1;
+        } else {
+          height = athlete.height - e - 1;
+        }
+        return Math.floor(height / 12) + "'" + (height % 12) + '"';
+      });
+      answers.splice(correctIx, 0, Math.floor(athlete.height / 12) + "'" + (athlete.height % 12) + '"');
+
+      sendQuestion("How tall is " + athlete.displayName + "?", answers, correctIx);
     };
+
+  questions = [
+    weightQuestion,
+    ageQuestion,
+    heightQuestion
+  ];
 
   // when a keyword is matched in the captions, a "lightning" round begins
   setInterval(lightningRound, 1000);
@@ -83,8 +144,7 @@ module.exports = function(){
       var knicks = roster;
 
       // send new trivia questions to all the players
-      setInterval(weightQuestion(celtics, knicks), 3000);
+      setInterval(chooseQuestion(celtics, knicks), 3000);
     });
   });
-  
 };
