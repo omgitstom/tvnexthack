e294727da4aa796e64164592192260f69d26eb5b
Matthew McClure <mmcclure@brightcove.com>
2013-04-27 14:03:49 -0700

    add sendQuestion

--- srv/pubsub.js
+++ srv/pubsub.js
@@ -3,11 +3,15 @@ var Firebase = require('firebase')
 
 module.exports = {
   drink: function(event, length) {
-    console.log('Drink!!1!');
     mainRef.child(event).child('drink').set(true);
     setTimeout(function() {
       mainRef.child(event).child('drink').set(false);
     }, length * 1000);
   },
-  sendQuestion(question, answers, correctIx){}
+  sendQuestion: function(question, answers, correctIx) {
+    var trivia = new Firebase('https://drivia.firebaseio.com/trivia');
+    newQuestion = trivia.push();
+    newQuestion.set({question: question, answers: answers, correct: correctIx});
+  },
+  answerQuestion: function() {}
 };
