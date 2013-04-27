f9dae52d361f8ccc956b2fdc311ba9299f426d92
Tom Abbott <tabbott@brightcove.com>
2013-04-27 17:20:59 -0400

    Adding transistions and 2nd page

--- srv/public/js/main.js
+++ srv/public/js/main.js
@@ -1,23 +1,57 @@
 function Rinkd(options) {
 	if (!(this instanceof Rinkd)) {
-    	return new BrightcoveAdobePass(options);
+    	return new Rinkd(options);
   	}
 
 	this.options = options;
 }
 
 Rinkd.prototype.init = function (){
-	this.firebase = new Firebase(this.options.firebaseURL);
-	this.firebase.set('Hello World!');
+	this.firebase = new Firebase(this.options.fireBaseURL);
+	this.authClient = new FirebaseAuthClient(this.firebase,  this.did_login.bind(this));
+	//wire up interface
+	$('.play-now').click(this.authenticate.bind(this));
+	$('.logout').click(this.logout.bind(this));
 };
 
+Rinkd.prototype.authenticate = function(){
+	this.authClient.login('twitter');
+};
+Rinkd.prototype.logout = function (){
+	this.authClient.logout();
+}
+
+Rinkd.prototype.did_login = function (error, user){
+	if (error) {
+	    // an error occurred while attempting login
+	    console.log(error);
+	} else if (user) {
+	    // user authenticated with Firebase
+	    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+	    
+	    //add user name
+	    $('.username').text('@'+user.screen_name);
+
+	    //Show log out button
+	    $('.logout').show();
+
+	    //Slide Intro Down/Slide in Game
+	    $('.intro').fadeOut(function(){$('.game').slideDown('slow')});
+
+
+	} else {
+		$('.username').text('');
+		$('.game').slideUp('slow', function(){$('.intro').fadeIn()});
+	    $('.logout').hide();
+	}
+}
 /**
 To bootstrap the creation of Rinkd
 **/
 $(document).ready(function(){
 	//Set up the options
 	var options = {
-		'fireBaseURL':''
+		'fireBaseURL':'https://drivia.firebaseio.com'
 	},
 	//Create a new object
 	drink = new Rinkd(options);
