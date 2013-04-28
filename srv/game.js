832c3e9705f6c03e85a1ce1387260c173aab0cd5
David LaPalomento <dlapalomento@gmail.com>
2013-04-27 22:34:42 -0400

    format money string in salary question

--- srv/game.js
+++ srv/game.js
@@ -167,12 +167,23 @@ module.exports = function(){
     salaryQuestion = function(team, athlete){
       var
         correctIx = random(0, 3),
-        answers = randoms(1, 10, 3);
+        answers = randoms(1, 10, 3),
+        // http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+        formatMoney = function(n, c, d, t){
+          var
+            c = isNaN(c = Math.abs(c)) ? 2 : c, 
+            d = d == undefined ? "." : d, 
+            t = t == undefined ? "," : t, 
+            s = n < 0 ? "-" : "", 
+            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+            j = (j = i.length) > 3 ? j % 3 : 0;
+          return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+        };
 
       answers = answers.map(function(e){
-        return '$' + (random(0, 1) ? athlete.stats.salary + (e * 10000) : athlete.stats.salary - (e * 10000));
+        return '$' + formatMoney((random(0, 1) ? athlete.stats.salary + (e * 10000) : athlete.stats.salary - (e * 10000)));
       });
-      answers.splice(correctIx, 0, '$' + athlete.stats.salary);
+      answers.splice(correctIx, 0, '$' + formatMoney(athlete.stats.salary));
 
       sendQuestion("How much is " + athlete.displayName + " making in the 2013 season?", answers, correctIx);
     },
