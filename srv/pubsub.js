604d004ed4cc6c51a8d49eea7eabe1db4f589dc1
Matthew McClure <mmcclure@brightcove.com>
2013-04-28 11:23:21 -0700

    redoing some if statements

--- srv/pubsub.js
+++ srv/pubsub.js
@@ -31,7 +31,7 @@ var func = {
     usersRef.child(user).child('drinks').once('value', function(snapshot){
       var currentDrinks = snapshot.val();
       // If currentPoints is null, set to 1, otherwise increment by 1
-      currentDrinks ? currentDrinks = currentDrinks + 1 : currentDrinks = 1;
+      currentDrinks = currentDrinks ? currentDrinks + 1 : 1;
       usersRef.child(user).child('drinks').set(currentDrinks);
     });
   },
@@ -53,8 +53,8 @@ var func = {
     },
     tallyPrevious: function(lastAnswers, answer) {
       console.log(lastAnswers);
-
-      for(var user in lastAnswers){
+      if(!lastAnswers) { return; }
+      Object.keys(lastAnswers).forEach(function(user){
         var lastAnswer = lastAnswers[user];
         console.log({user: user, answer: lastAnswers[user]});
         usersRef.child(user).child('points').once('value', function(snapshot){
@@ -62,19 +62,20 @@ var func = {
           if (lastAnswer == answer) {
             console.log('Correct!');
             // If currentPoints is null, set to 1, otherwise increment by 1
-            currentPoints ? currentPoints = currentPoints + 1 : currentPoints = 1;
+            currentPoints = currentPoints ? currentPoints + 1 : 1;
             //console.log({user: userName, answer: userAnswer, correct: true, score: currentPoints});
           } else {
             console.log('Incorrect!');
             // decrement by 1 if currentPoints is greater than 0, otherwise set to 0
-            currentPoints > 0 ? currentPoints = currentPoints - 1 : currentPoints = 0;
+            currentPoints = currentPoints > 0 ? currentPoints - 1 : 0;
             //console.log({user: userName, answer: userAnswer, correct: false, score: currentPoints});
             func.oneDrink(user);
           }
           // Set the value for the child.
           usersRef.child(user).child('points').set(currentPoints);
+          usersRef.child(user).setPriority(10000 - currentPoints);
         });
-      }
+      });
     }
   }
 };
